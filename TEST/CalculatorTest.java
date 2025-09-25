6ab4153467ff7e2030ca5fac6676d06984d3a8c3
parkjiwoo <bagjiw@naver.com>
2025-09-25 11:19:49 +0900

    계산기1.0v 소스파일

---
+++ TEST/CalculatorTest.java
@@ -0,0 +1,36 @@
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class CalculatorTest {
+
+    @Test
+    void add() {
+        Calculator calc = new Calculator();
+        calc.setNumbers(10, 20);
+        assertEquals(30, calc.add());
+    }
+
+    @Test
+    void subtract() {
+        Calculator calc = new Calculator();
+        calc.setNumbers(20,10);
+        assertEquals(10, calc.subtract());
+
+    }
+
+     @Test
+     void multiply() {
+         Calculator calc = new Calculator();
+         calc.setNumbers(10,5);
+         assertEquals(50, calc.multiply());
+
+    }
+
+    @Test
+    void divide() {
+        Calculator calc = new Calculator();
+        calc.setNumbers(60,6);
+        assertEquals(0,calc.divide());
+    }
+}
