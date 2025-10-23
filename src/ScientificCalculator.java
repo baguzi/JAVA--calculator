112d2493a85c61b80a025a857053982e40daff4a
parkjiwoo <bagjiw@naver.com>
2025-10-24 02:40:34 +0900

    result 값을 불러와서 sum 에 저장 2진수를 담을 문자열 a 생성

--- src/ScientificCalculator.java
+++ src/ScientificCalculator.java
@@ -1,2 +1,16 @@
-public class ScientificCalculator {
+public class ScientificCalculator extends AdvancedCalculator {
+
+    public void binaryChanges(){
+        int sum = result;
+        String a = "" ;
+        while (sum> 0){
+            int bit = sum % 2;
+
+        };
+
+
+
+
+    }
+
 }
