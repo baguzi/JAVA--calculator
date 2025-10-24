6f04c5aa589c2ceb28f01a22e72a3a3ad7bbc0ab
parkjiwoo <bagjiw@naver.com>
2025-10-24 15:19:10 +0900

    divide 를 이용해 몫을 갱신하여 나눗셈을 이어간다

--- src/ScientificCalculator.java
+++ src/ScientificCalculator.java
@@ -1,12 +1,22 @@
 public class ScientificCalculator extends AdvancedCalculator {
 
-    public void binaryChanges(){
+    public String binaryChanges() {
         int sum = result;
-        String a = "" ;
-        while (sum> 0){
+        String a = "";
+        while (sum > 0) {
             int bit = sum % 2;
+            a = bit + a;
+            num1 = sum;
+            num2 = 2;
+            divide();
+            sum = result;
 
-        };
+        }
+        return a;
+    }
+
+
+
 
 
 
